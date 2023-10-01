6bfa1cad1d8ba03c586d02b1b34663d2c326d703
hamza-walker <hamzawalker@Hamzas-MacBook-Pro.local>
2023-10-01 14:30:04 +0200

    trying to debug the issue with the page not loading. added CORS for the getAllRooms method

--- src/main/java/com/codecool/hogwartshouses/controller/RoomController.java
+++ src/main/java/com/codecool/hogwartshouses/controller/RoomController.java
@@ -5,6 +5,7 @@ import com.codecool.hogwartshouses.model.Room;
 import com.codecool.hogwartshouses.service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,6 +24,7 @@ public class RoomController {
     }
 
     @GetMapping("/rooms") // Unique path for the getAllRooms method
+    @CrossOrigin(origins = "http://localhost:3000") // Allow requests from http://localhost:3000
     @ResponseBody // Return the response as JSON
     public List<Room> getAllRooms() {
         return roomService.getAllRooms();
